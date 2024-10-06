6d7b8a0f732f21d17f3a0d880e0aac0939893bdf
NicoleCastiglione <nicolecastiglione0@gmail.com>
2024-10-06 20:32:31 +0000

    ADD: Se agrega el punto B completo

---
+++ RTA_SCRIPT_Examen_20241006/Punto_B.sh
@@ -0,0 +1,60 @@
+sudo fdisk -l
+fdisk /dev/vdb
+sudo fdisk /dev/vdb
+sudo fdisk -l /dev/vdb
+mkfs.ext4 /dev/vdb1 /dev/vdb2
+sudo fdisk -l /dev/vdb
+mkfs.ext4 /dev/vdb1
+sudo mkfs.ext4 /dev/vdb1
+sudo fdisk -l /dev/vdb
+sudo mkfs.ext4 /dev/vdb2
+sudo mkfs.ext4 /dev/vdb3
+sudo mkfs.ext4 /dev/vdb5
+sudo mkfs.ext4 /dev/vdb6
+sudo mkfs.ext4 /dev/vdb7
+sudo mkfs.ext4 /dev/vdb8
+sudo mkfs.ext4 /dev/vdb9
+sudo mkfs.ext4 /dev/vdb10
+sudo mkfs.ext4 /dev/vdb11
+lsblk
+lsblk -f
+sudo vim /etc/fstab
+ls
+cd repogit/
+ls
+cd UTNFRA_SO_1P2C_2024_Castiglione/
+ls
+cd RTA_ARCHIVOS_Examen_20241006/
+LS
+ls
+cd ..
+ls
+cd /Examenes-UTN/
+ls
+tree .
+cd alumno_1/parcial_1
+pwd
+lsblk -f
+clear
+lsblk -f
+clear
+cd /Examenes-UTN/alumno_1/parcial_1/
+clear
+pwd
+clear
+lsblk -f
+clear
+sudo fdisk -l /dev/vdb
+clear
+cd
+clear
+sudo vim /etc/fstab
+cat /etc/fstab
+mount -a
+sudo mount -a
+clear
+sudo mount -a
+clear
+cat /etc/fstab
+clear
+lsblk -f
