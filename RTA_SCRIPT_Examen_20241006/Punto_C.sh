da873db85bc9a87a79cbe1286e0741a9d7ad131b
NicoleCastiglione <nicolecastiglione0@gmail.com>
2024-10-07 20:04:26 +0000

    ADD: Se agrega el punto C

---
+++ RTA_SCRIPT_Examen_20241006/Punto_C.sh
@@ -0,0 +1,84 @@
+sudo groupadd p1c2_2024_gAlumno
+sudo groupadd p1c2_2024_gProfesores
+bash Scripts/crear_usuario.sh
+bash Scripts/obtener_password.sh nicolecastiglione
+bash Scripts/obtener_password.sh p1c2_2024_A1
+bash Scripts/crear_usuario.sh
+history
+cd repogit/UTNFRA_SO_1P2C_2024_Castiglione/
+ls
+cd RTA_SCRIPT_Examen_20241006/
+ls
+ll
+cd ..
+ls -l /Examenes-UTN/alumno_
+cd ..
+ls -l /Examenes-UTN/alumno_
+ls -l Examenes-UTN/_
+ls -l Examenes-UTN
+cd ~
+pwd
+chown p1c2_2024_A1:p1c2_2024_A1 /Examenes-UTN/alumno_1
+cd ..
+ls -l Examenes-UTN
+cd
+cd ~
+sudo chown p1c2_2024_A1:p1c2_2024_A1 /Examenes-UTN/alumno_1
+cd ..
+ls -l Examenes-UTN
+cd ~
+pws
+pwd
+sudo chown p1c2_2024_A2:p1c2_2024_A2 /Examenes-UTN/alumno_2
+sudo chown p1c2_2024_A3:p1c2_2024_A3 /Examenes-UTN/alumno_3
+sudo chown p1c2_2024_P1:p1c2_2024_gProfesores /Examenes-UTN/profesores
+cd ..
+ls -l Examenes-UTN/
+cd ~
+pwd
+sudo chmod 770 /Examenes-UTN/alumno_1
+sudo ls -l /Examenes-UTN/alumno_1
+cd ..
+ls -l E
+ls -l Examenes-UTN/
+cd ~
+sudo chmod 750 /Examenes-UTN/alumno_1
+cd ..
+ls -l Examenes-UTN/
+cd ~
+sudo chmod 760 /Examenes-UTN/alumno_2
+sudo chmod 700 /Examenes-UTN/alumno_3
+sudo chmod 775 /Examenes-UTN/profesores
+cd ..
+ls -l Examenes-UTN/
+ls -l Examenes-UTN/alumno_1
+cd ~
+sudo chmod -R 750 /Examenes-UTN/alumno_1
+sudo chmod -R 760 /Examenes-UTN/alumno_2
+sudo chmod -R 700 /Examenes-UTN/alumno_3
+sudo chmod -R 775 /Examenes-UTN/profesores
+cd ..
+ls -l Examenes-UTN/
+cd Examenes-UTN/alumno_1
+cd ~
+cd repogit/UTNFRA_SO_1P2C_2024_Castiglione/
+cd ..
+cd repogit/UTNFRA_SO_1P2C_2024_Castiglione/
+git add .
+git commit -m "ADD: Se agrega el punto C sin el archivo validar.txt"
+git push
+cd ..
+git add .
+cd UTNFRA_SO_1P2C_2024_Castiglione/
+git add .
+clear
+cd ..
+sudo su - p1c2_2024_A1 -c "whoami > /Examenes-UTN/alumno_1/validar.txt"
+sudo cat /Examenes-UTN/alumno_1/validar.txt
+sudo su -c "whoami > /Examenes-UTN/alumno_2/validar.txt" p1c2_2024_A2
+sudo cat /Examenes-UTN/alumno_2/validar.txt
+sudo su -c "whoami > /Examenes-UTN/alumno_3/validar.txt" p1c2_2024_A3
+sudo cat /Examenes-UTN/alumno_3/validar.txt
+sudo su -c "whoami > /Examenes-UTN/profesores/validar.txt" p1c2_2024_P1
+sudo cat /Examenes-UTN/profesores/validar.txt
+sudo su -c "whoami >> /Examenes-UTN/alumno_1/validar.txt" p1c2_2024_P1
