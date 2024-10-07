89aba5beecf8ed87daf54297f691d9a529f29e71
NicoleCastiglione <nicolecastiglione0@gmail.com>
2024-10-07 22:10:59 +0000

    ADD: Se agrega el punto E

---
+++ RTA_SCRIPT_Examen_20241006/Punto_E.sh
@@ -0,0 +1,2 @@
+grep MemTotal /proc/meminfo > ~/repogit/UTNFRA_SO_1P2C_2024_Castiglione/RTA_ARCHIVOS_Examen_20241006/Filtro_Basico.txt
+sudo dmidecode -t chassis | grep Manufacturer >> repogit/UTNFRA_SO_1P2C_2024_Castiglione/RTA_ARCHIVOS_Examen_20241006/Filtro_Basico.txt
